62d3623035937bfc47e4831a046aef5967455cc8
BTC <bturgaycalik@gmail.com>
2020-07-01 18:38:56 +0300

    verison2

--- algoritmbase.m
+++ algoritmbase.m
@@ -2,5 +2,3 @@
 PVGEN=zeros(1,66);  % 33 baralý sistem için 66 satýrdan oluþan PV ek güç ve güç katsayýsý matrisi
 
 [Flow, Flowlose]=NRLFA1(PVGEN); %Sisteme hiç kaynak eklenmediði ve optimizasyonsuz çýktýdsadasd
-
-%%dasdasdas
